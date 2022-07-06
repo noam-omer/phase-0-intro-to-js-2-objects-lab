ac5372db5f4aeaebe8ee88b80c3f0da7cd14f320
Noam Fischhoff <nfischhoff@gmail.com>
2022-07-05 20:46:35 -0400

    commit tree 3336818203aa74768596e83b679295d2f055694f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: 'John Doe',
+    address: '450 Pearl St.',
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    console.log(newEmployee);
+    delete newEmployee[key];
+    console.log(newEmployee);
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
+console.log("test");
